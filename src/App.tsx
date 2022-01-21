10e5fb5bd2f9c2bd1ad51266ccda358ac7002163
dhaifandito <dhaifan.da@gmail.com>
2022-01-21 20:21:28 +0700

    add homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div style={{height:"10vh"}}></div>
+      <Home />
       <div style={{height:"100vh"}}></div>
       <Footer />
     </div>
---
+++ src/components/HomeCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface Props {
+    image: string
+    title: string
+    price: string
+}
+
+function HomeCard(props: Props) {
+    return (
+        <div className="col-md-3 p-3 d-flex justify-content-center">
+            <div className='HomeCard'>
+            <div className='imageHolder' style={{ width: "250px", height: "300px" }}>
+                <img src={props.image} className='w-100 h-100' />
+            </div>
+            <div className='d-flex justify-content-between px-2'>
+            <div className='homeInfo text-left mt-2' style={{font:"18px"}}>
+                <p>{props.title}</p>
+                <p>Rp.{props.price}</p>
+            </div>
+            <button className='btn btn-success btn-sm h-50 m-2'>Add to Cart</button>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomeCard;
