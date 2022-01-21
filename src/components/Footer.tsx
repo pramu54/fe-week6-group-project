9ef60979d9da8560791cb5669ff5c861c6cb701b
dhaifandito <dhaifan.da@gmail.com>
2022-01-21 19:03:11 +0700

    add header and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="Footer w-100 h-auto py-3" style={{backgroundColor: "mediumturquoise"}}>
+        <p style={{color: "white"}}>&copy; 2022 SIRCLO Tech Academy </p>
+    </div>
+  );
+}
+
+export default Footer;
