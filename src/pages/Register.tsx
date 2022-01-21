fc37f0ccddddbb4a35588717e0e2c66e32ded3a5
prime_ion <mohadipramudiono@gmail.com>
2022-01-21 20:03:27 +0700

    initialized register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Register = () => {
     return(
-        <></>
+        <div className="container" style={{height:"100vh"}}>
+            <h1>Register</h1>
+        </div>
     )
 }
 
